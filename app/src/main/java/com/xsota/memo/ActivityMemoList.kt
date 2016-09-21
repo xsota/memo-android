ef2df622793b5dd54f822d0a50c40f4df44029dd
sota <git@xsota.com>
2016-09-22 00:14:03 +0900

    use lazy

--- app/src/main/java/com/xsota/memo/ActivityMemoList.kt
+++ app/src/main/java/com/xsota/memo/ActivityMemoList.kt
@@ -4,23 +4,21 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-
 import com.xsota.memo.databinding.ActivityMainBinding
 
 class ActivityMemoList : AppCompatActivity() {
 
-    private var mBinding: ActivityMainBinding? = null
-
+    private val mBinding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        setSupportActionBar(mBinding!!.toolbar)
-        mBinding!!.includedContent.listview.emptyView = mBinding!!.includedContent.emptyView
+        setSupportActionBar(mBinding.toolbar)
+        mBinding.includedContent.listview.emptyView = mBinding.includedContent.emptyView
 
-        mBinding!!.fab.setOnClickListener {
+        mBinding.fab.setOnClickListener {
             val i = Intent(this@ActivityMemoList, ActivityEditMemo::class.java)
             startActivity(i)
         }
---
+++ app/src/main/java/com/xsota/memo/App.kt
@@ -0,0 +1,14 @@
+package com.xsota.memo
+
+import android.app.Application
+
+/**
+ * Created by sota on 2016/09/21.
+ */
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
