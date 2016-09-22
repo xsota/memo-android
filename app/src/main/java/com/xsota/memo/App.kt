9aa79d5ec3758def0c1d5f4701e079b21b767229
sota <git@xsota.com>
2016-09-22 23:20:51 +0900

    initialize realm

--- app/src/main/java/com/xsota/memo/App.kt
+++ app/src/main/java/com/xsota/memo/App.kt
@@ -1,6 +1,8 @@
 package com.xsota.memo
 
 import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 /**
  * Created by sota on 2016/09/21.
@@ -10,5 +12,12 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
+
+        // Initialize Realm
+        val realmConfig = RealmConfiguration.Builder(this)
+                .deleteRealmIfMigrationNeeded()
+                .build()
+
+        Realm.setDefaultConfiguration(realmConfig)
     }
 }
