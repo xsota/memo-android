dd41f8ba2e2d0a8aca57937659fcac3cfc53a457
sota <git@xsota.com>
2016-09-22 23:21:59 +0900

    getMemoList

--- app/src/main/java/com/xsota/memo/models/Memo.kt
+++ app/src/main/java/com/xsota/memo/models/Memo.kt
@@ -1,6 +1,8 @@
 package com.xsota.memo.models
 
+import io.realm.Realm
 import io.realm.RealmObject
+import io.realm.RealmResults
 import io.realm.annotations.PrimaryKey
 
 /**
@@ -11,4 +13,13 @@ open class Memo : RealmObject() {
     @PrimaryKey var id: String? = null
     var title: String? = null
     var body: String? = null
+
+    companion object {
+        fun getMemoList(): RealmResults<Memo> {
+            val realm = Realm.getDefaultInstance()
+            val result = realm.where(Memo::class.java).findAll()
+
+            return result
+        }
+    }
 }
