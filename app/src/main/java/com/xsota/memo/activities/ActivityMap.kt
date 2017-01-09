660d3d13193bf7a20c639fd2fc7c5035b5bac7fe
sota <git@xsota.com>
2017-01-09 15:18:47 +0900

    added map activity

---
+++ app/src/main/java/com/xsota/memo/activities/ActivityMap.kt
@@ -0,0 +1,13 @@
+package com.xsota.memo.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.xsota.memo.R
+
+class ActivityMap : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
