89db4280ef643dadb2bb1a5c240b8c35c51590f3
sota <git@xsota.com>
2017-01-10 22:02:40 +0900

    create EditMemoViewModel

---
+++ app/src/main/java/com/xsota/memo/viewmodels/EditMemoViewModel.kt
@@ -0,0 +1,12 @@
+package com.xsota.memo.viewmodels
+
+import android.app.Activity
+
+/**
+ * Created by sota on 2017/01/10.
+ */
+class EditMemoViewModel(activity:Activity){
+    private val mActivity = activity
+    
+}
+
